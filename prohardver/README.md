088308c6b1b2a408444fd43f2f3b597414e7b04e
nakiakira <fazekmar@gmail.com>
2019-03-23 10:54:45 +0100

    fix urls

--- prohardver/README.md
+++ prohardver/README.md
@@ -1,4 +1,4 @@
 # Prohardver lapcsaladdal kapcsolatos scriptek
 
-##### RangValaszto - [Telepites](https://raw.githubusercontent.com/fazekmar/browser-userscripts/master/prohardver/rangvalaszto.js)
+##### RangValaszto - [Telepites](https://raw.githubusercontent.com/fazekmar/browser-userscripts/master/prohardver/rangvalaszto.user.js)
 Lecsereli a felhasznalo(k) / rang(ok) szovegezeset a Prohardver lapcsalad foruman.
--- prohardver/rangvalaszto.user.js
+++ prohardver/rangvalaszto.user.js
@@ -5,7 +5,7 @@
 // @author       https://github.com/fazekmar
 // @license      GPL-3.0; https://github.com/fazekmar/browser-userscripts/blob/master/LICENSE
 // @description  Lecsereli a felhasznalo(k) / rang(ok) szovegezeset a Prohardver lapcsalad foruman.
-// @updateURL    https://raw.githubusercontent.com/fazekmar/browser-userscripts/master/prohardver/rangvalaszto.js
+// @updateURL    https://raw.githubusercontent.com/fazekmar/browser-userscripts/master/prohardver/rangvalaszto.user.js
 // @include      /.*fototrend.hu/(tema|privat|bejegyzes).*$/
 // @include      /.*prohardver.hu/(tema|privat|bejegyzes).*$/
 // @include      /.*mobilarena.hu/(tema|privat|bejegyzes).*$/

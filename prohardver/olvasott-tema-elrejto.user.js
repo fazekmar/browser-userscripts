f8f0cc23ff10c11200a8bb1d8cfb01d444a54ec9
fazekmar <fazekmar@gmail.com>
2021-03-19 21:25:35 +0100

    Export helper function to a library

--- prohardver/olvasott-tema-elrejto.user.js
+++ prohardver/olvasott-tema-elrejto.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Olvasott tema elrejto
 // @namespace    https://github.com/fazekmar/browser-userscripts
-// @version      0.1598688166
+// @version      0.1616176831
 // @author       https://github.com/fazekmar
 // @license      GPL-3.0; https://github.com/fazekmar/browser-userscripts/blob/master/LICENSE
 // @description  Elrejti azokat a temakat ahova nem erkezett uj hozzaszolas a Kedvencek/Itt szoltam hozza listakbol. A fejlecekre kattintva ujra elo lehet hozni/el lehet rejteni oket
@@ -12,8 +12,11 @@
 // @include      /.*gamepod.hu/(forum|tema|privat|tag).*$/
 // @include      /.*itcafe.hu/(forum|tema|privat|tag).*$/
 // @run-at       document-idle
+// @require      https://raw.githubusercontent.com/fazekmar/browser-userscripts/master/utils/display-element.user.js
 // @grant        none
 // ==/UserScript==
+
+/* globals displayElement */
 
 const placeOnClickToHeader = (userThreadList) => {
   const cardHeader = userThreadList.getElementsByClassName("card-header");
@@ -23,7 +26,7 @@ const placeOnClickToHeader = (userThreadList) => {
       header.innerText.indexOf("Kedvenc fórumtémáim") !== -1 ||
       header.innerText.indexOf("Itt szóltam hozzá") !== -1
     ) {
-      header.onclick = () => onClick(header, userThreadList, false);
+      header.onclick = () => onClick(header, userThreadList, true);
       header.style.cursor = "pointer";
     }
   });
@@ -38,37 +41,12 @@ const onClick = (onClickElement, watchElement, show) => {
   }
 };
 
-const toggleItems = (doc, show = true) =>
+const toggleItems = (doc, show = false) =>
   [...doc.getElementsByClassName("list-group-item")].forEach((item) => {
     if (item.getElementsByClassName("new-msgs").length === 0) {
-      toggleElement(item, show, "block", true);
+      displayElement(item, show, "block", true);
     }
   });
-
-const toggleElement = (element, state, display, fade = false) => {
-  const intervalSpeed = 25;
-  try {
-    if (fade) {
-      let opacity = state ? 1.0 : 0.0;
-      element.style.opacity = opacity;
-      if (!state) {
-        element.style.display = display;
-      }
-      const interval = setInterval(() => {
-        opacity += state ? -0.1 : 0.1;
-        element.style.opacity = opacity;
-        if (state ? opacity <= 0 : opacity >= 1) {
-          element.style.display = state ? "none" : display;
-          clearInterval(interval);
-        }
-      }, intervalSpeed);
-    } else {
-      element.style.display = state ? "none" : display;
-    }
-  } catch (e) {
-    console.log(e);
-  }
-};
 
 (function () {
   try {
---
+++ utils/display-element.user.js
@@ -0,0 +1,62 @@
+// ==UserScript==
+// @name         DisplayElement
+// @namespace    https://github.com/fazekmar/browser-userscripts
+// @version      0.1616176831
+// @author       https://github.com/fazekmar
+// @license      GPL-3.0; https://github.com/fazekmar/browser-userscripts/blob/master/LICENSE
+// @description  Set element display property and optionally fading it
+// @updateURL    https://raw.githubusercontent.com/fazekmar/browser-userscripts/master/utils/display-element.user.js
+// @grant        none
+// ==/UserScript==
+
+/**
+ * Set element display property and optionally fading it
+ *
+ * @param {HTMLElement} element
+ * @param {boolean} isDisplay - Show/hide element
+ * @param {string} [display=""] - CSS display property used when isDisplay is true
+ * @param {boolean} [fade=false] - Enable/disable fade effect
+ * @param {number} [intervalSpeed=25] - Fade speed in milliseconds
+ * @param {number} [rate=0.1] - Fade increment/decrement rate between 0 and 1
+ */
+function displayElement(
+  element,
+  isDisplay,
+  display = "",
+  fade = false,
+  intervalSpeed = 25,
+  rate = 0.1,
+) {
+  try {
+    if (fade) {
+      let opacity, current_rate;
+
+      if (isDisplay) {
+        opacity = 0.0;
+        current_rate = rate;
+
+        element.style.opacity = opacity;
+        element.style.display = display;
+      } else {
+        opacity = 1.0;
+        current_rate = -rate;
+      }
+
+      const interval = setInterval(() => {
+        opacity += current_rate;
+
+        if (isDisplay ? opacity >= 1 : opacity <= 0) {
+          opacity = isDisplay ? 1.0 : 0.0;
+          element.style.display = isDisplay ? display : "none";
+          clearInterval(interval);
+        }
+
+        element.style.opacity = opacity;
+      }, intervalSpeed);
+    } else {
+      element.style.display = isDisplay ? display : "none";
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
